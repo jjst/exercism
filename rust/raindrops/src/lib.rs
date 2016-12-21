e6bcc363a67f19b12015359eae295b6a266afc16
Jeremie Jost <jeremiejost@gmail.com>
2016-12-21 23:28:06 +0000

    Rust: raindrops

---
+++ rust/raindrops/src/lib.rs
@@ -0,0 +1,19 @@
+
+pub fn raindrops(number: u32) -> String {
+    let mut r = String::new();; //String::from("Hello, ");
+    if number % 3 == 0 {
+        r.push_str("Pling");
+    }
+    if number % 5 == 0 {
+        r.push_str("Plang");
+    }
+    if number % 7 == 0 {
+        r.push_str("Plong");
+    }
+    if r.is_empty() {
+        number.to_string()
+    } else {
+        r
+    }
+}
+
