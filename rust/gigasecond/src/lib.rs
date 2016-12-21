1e474beaeb5f4192a7c822bf2479cc665b9ced58
Jeremie Jost <jeremiejost@gmail.com>
2016-12-21 23:38:12 +0000

    Better error message

--- rust/gigasecond/src/lib.rs
+++ rust/gigasecond/src/lib.rs
@@ -2,6 +2,7 @@ extern crate chrono;
 use chrono::*;
 
 pub fn after(date: DateTime<UTC>) -> DateTime<UTC> {
-    date.checked_add(Duration::seconds(1000000000)).expect("oh crap")
+    date.checked_add(Duration::seconds(1000000000))
+        .expect("overflow in checked add")
 }
 
