a8d0efe9d0c7522e4af0b320a4edec3a3e0e7aa7
Jeremie Jost <jeremiejost@gmail.com>
2017-03-30 23:02:17 +0100

    beer-song tdd-style: first test passing

---
+++ rust/beer-song/src/lib.rs
@@ -0,0 +1,8 @@
+
+pub fn verse(bottle_count: u8) -> String {
+    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string()
+}
+
+pub fn sing(start: u8, end: u8) -> String {
+    String::from("")
+}
