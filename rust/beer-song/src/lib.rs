1f889037524e6a15816ec8d8bd2ccbccf1e916bb
Jeremie Jost <jeremiejost@gmail.com>
2017-04-04 22:54:16 +0100

    beer song tdd style: third test passing

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -1,6 +1,8 @@
 
 pub fn verse(bottle_count: u8) -> String {
-    if bottle_count > 0 {
+    if bottle_count > 1 {
+        "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string()
+    } else if bottle_count > 0 {
         "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string()
     } else {
         "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string()
