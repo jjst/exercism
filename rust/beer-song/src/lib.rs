64c220e7b2d534486e3ee1fb7707720ec4d3d0b6
Jeremie Jost <jeremiejost@gmail.com>
2017-04-12 18:40:27 +0100

    beer-song: refactor, remove string duplication

--- rust/beer-song/src/lib.rs
+++ rust/beer-song/src/lib.rs
@@ -9,19 +9,16 @@ pub fn fmt_bottle_count(bottle_count: u8) -> String {
     }
 }
 pub fn verse(bottle_count: u8) -> String {
-    if bottle_count > 2 {
+    if bottle_count > 0 {
         let current_bcount = fmt_bottle_count(bottle_count);
         let new_bcount = fmt_bottle_count(bottle_count - 1);
         format!(
-            "{} of beer on the wall, {} of beer.\nTake one down and pass it around, {} of beer on the wall.\n",
+            "{} of beer on the wall, {} of beer.\nTake {} down and pass it around, {} of beer on the wall.\n",
             current_bcount,
             current_bcount,
+            if bottle_count == 1 { "it" } else { "one" },
             new_bcount
         )
-    } else if bottle_count > 1 {
-        "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n".to_string()
-    } else if bottle_count > 0 {
-        "1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n".to_string()
     } else {
         "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n".to_string()
     }
