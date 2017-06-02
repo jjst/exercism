e1a3fa97ac64b03095912af33a82be6a97e34705
Jeremie Jost <jeremiejost@gmail.com>
2017-06-02 23:17:03 +0100

    sum of multiples, naive brute force solution

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,9 +1,10 @@
 
 pub fn sum_of_multiples(upper_bound: u16, numbers: &Vec<u16>) -> u64 {
-    let mut sum: u64 = 0;
-    for &n in numbers {
-        if n > upper_bound { break; }
-        sum += n as u64;
+    let mut total_sum: u64 = 0;
+    for n in 1..upper_bound {
+        if numbers.iter().any(|&x| n % x == 0) {
+            total_sum += n as u64;
+        }
     }
-    sum
+    total_sum
 }
