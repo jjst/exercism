f8a3f11ca7ca89a4ff3d010fbe31a3dedb462335
Jeremie Jost <jeremiejost@gmail.com>
2017-06-02 23:02:26 +0100

    Pass second test

--- rust/sum-of-multiples/src/lib.rs
+++ rust/sum-of-multiples/src/lib.rs
@@ -1,4 +1,9 @@
 
 pub fn sum_of_multiples(upper_bound: u16, numbers: &Vec<u16>) -> u64 {
-    0
+    let mut sum: u64 = 0;
+    for &n in numbers {
+        if n > upper_bound { break; }
+        sum += n as u64;
+    }
+    sum
 }
