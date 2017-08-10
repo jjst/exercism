51a82aebdc115fcbfa82f587f0c928a6655b534c
Jeremie Jost <jeremiejost@gmail.com>
2017-08-10 20:57:06 +0100

    Rust proverb: all tests passing

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -3,6 +3,10 @@ pub fn build_proverb(pieces: Vec<&str>) -> String {
     for (w1, w2) in pieces.iter().zip(pieces.iter().skip(1)) {
         sentences.push(format!("For want of a {} the {} was lost.", w1, w2));
     }
-    sentences.push(format!("And all for the want of a {}.", pieces.first().unwrap()));
+    if pieces.len() >= 3 {
+        sentences.push(format!("And all for the want of a {}{} {}.", pieces[2], pieces[1], pieces[0]));
+    } else if pieces.len() >= 1 {
+        sentences.push(format!("And all for the want of a {}.", pieces[0]));
+    }
     sentences.join("\n")
 }
