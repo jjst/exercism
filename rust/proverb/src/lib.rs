019e108aa938c284e27c1d066c59e7b8a5f04d10
Jeremie Jost <jeremiejost@gmail.com>
2017-08-13 20:14:46 +0100

    Use map() and collect()

--- rust/proverb/src/lib.rs
+++ rust/proverb/src/lib.rs
@@ -1,8 +1,10 @@
 pub fn build_proverb(pieces: Vec<&str>) -> String {
-    let mut sentences = Vec::with_capacity(pieces.len());
-    for (w1, w2) in pieces.iter().zip(pieces.iter().skip(1)) {
-        sentences.push(format!("For want of a {} the {} was lost.", w1, w2));
-    }
+    let mut sentences: Vec<String> =
+        pieces
+            .iter()
+            .zip(pieces.iter().skip(1))
+            .map(|(w1, w2)| format!("For want of a {} the {} was lost.", w1, w2))
+            .collect();
     if pieces.len() >= 3 {
         sentences.push(format!("And all for the want of a {}{} {}.", pieces[2], pieces[1], pieces[0]));
     } else if pieces.len() >= 1 {
