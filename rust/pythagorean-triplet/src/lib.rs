bed31ea2e8605eb0a5796c34dd6f581f7bf42bb4
Jeremie Jost <jeremiejost@gmail.com>
2017-09-29 21:08:54 +0100

    rust pythagorean triplets passing

--- rust/pythagorean-triplet/src/lib.rs
+++ rust/pythagorean-triplet/src/lib.rs
@@ -1,3 +1,39 @@
 pub fn find() -> Option<u32> {
-    Some(31875000)
+    pythagorean_triplet_summing_to(1000).map(|(a, b, c)| a * b * c)
 }
+
+pub fn pythagorean_triplet_summing_to(sum: u32) -> Option<(u32, u32, u32)> {
+    triplets_summing_to(sum)
+        .find(|&(a, b, c)| is_pythagorean_triplet(a, b, c))
+}
+
+pub struct TripletsSummingTo {
+    curr: (u32, u32, u32),
+    sum: u32,
+}
+
+impl Iterator for TripletsSummingTo {
+    type Item = (u32, u32, u32);
+    
+    fn next(&mut self) -> Option<(u32, u32, u32)> {
+        let (a, b, c) = self.curr;
+        if b > a + 1 {
+            self.curr = (a + 1, b - 1, c);
+            Some((a, b, c))
+        } else if c > b {
+            self.curr = (1, self.sum - c, c - 1);
+            Some((a, b, c))
+        } else {
+            None
+        }
+    }
+}
+
+pub fn triplets_summing_to(sum: u32) -> TripletsSummingTo {
+    TripletsSummingTo { curr: (1, 1, sum - 2), sum: sum }
+}
+
+pub fn is_pythagorean_triplet(a: u32, b: u32, c: u32) -> bool {
+    a*a + b*b == c*c
+}
+
--- rust/pythagorean-triplet/tests/pythagorean-triplet.rs
+++ rust/pythagorean-triplet/tests/pythagorean-triplet.rs
@@ -1,6 +1,67 @@
 extern crate pythagorean_triplet;
 
 #[test]
+fn test_triplets_summing_to_initial() {
+    assert_eq!(
+        pythagorean_triplet::triplets_summing_to(1000).next(), 
+        Some((1, 1, 998))
+    );
+}
+
+#[test]
+fn test_triplets_summing_to_last() {
+    assert_eq!(
+        pythagorean_triplet::triplets_summing_to(1000).last(), 
+        Some((332, 335, 333))
+    );
+}
+
+
+#[test]
+fn test_triplets_summing_to_first_ten() {
+    for ts in pythagorean_triplet::triplets_summing_to(1000) {
+        println!("{:?}", ts);
+    }
+    let first_ten : Vec<(u32, u32, u32)> = 
+        pythagorean_triplet::triplets_summing_to(1000)
+            .take(10)
+            .collect();
+    assert_eq!(
+        first_ten,
+        vec![
+            (1, 1, 998), (1, 2, 997), (1, 3, 996), (2, 2, 996), (1, 4, 995),
+            (2, 3, 995), (1, 5, 994), (2, 4, 994), (3, 3, 994), (1, 6, 993)
+        ]
+    );
+}
+
+#[test]
+fn test_is_pythagorean_triplet() {
+    assert_eq!(pythagorean_triplet::is_pythagorean_triplet(3, 4, 5), true);
+}
+
+#[test]
+fn test_is_pythagorean_triplet_2() {
+    assert_eq!(pythagorean_triplet::is_pythagorean_triplet(9, 40, 41), true);
+}
+
+#[test]
+fn test_is_pythagorean_triplet_3() {
+    assert_eq!(pythagorean_triplet::is_pythagorean_triplet(5, 12, 13), true);
+}
+
+#[test]
+fn test_pythagorean_triplet_summing_to() {
+    let all: Vec<(u32, u32, u32)> = 
+        pythagorean_triplet::triplets_summing_to(30)
+            .collect();
+    println!("{:?}", all);
+    assert_eq!(
+        pythagorean_triplet::pythagorean_triplet_summing_to(30), Some((5, 12, 13))
+    );
+}
+
+#[test]
 fn test_answer() {
     assert_eq!(pythagorean_triplet::find(), Some(31875000));
 }
