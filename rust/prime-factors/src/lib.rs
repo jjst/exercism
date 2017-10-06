e511b57e9820574c94a76904e0765358c9b7f20e
Jeremie Jost <jeremiejost@gmail.com>
2017-10-06 19:44:39 +0100

    Prime factors: all tests passing

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -1,4 +1,15 @@
 
-pub fn factors(number: u32) -> Vec<u16> {
-    vec![]
+pub fn factors(number: u64) -> Vec<u64> {
+    let mut factors = Vec::new();
+    let mut current = number;
+    while current > 1 {
+        for i in 2..(current + 1) {
+            if current % i == 0 {
+                current = current / i;
+                factors.push(i);
+                break;
+            }
+        }
+    }
+    factors
 }
