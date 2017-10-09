8eb9a75a5e73f68e6b1459858b1d3cfa93ab0620
Jeremie Jost <jeremiejost@gmail.com>
2017-10-09 20:55:21 +0100

    Clean out/refactor prime-factors

--- rust/prime-factors/src/lib.rs
+++ rust/prime-factors/src/lib.rs
@@ -3,13 +3,18 @@ pub fn factors(number: u64) -> Vec<u64> {
     let mut factors = Vec::new();
     let mut current = number;
     while current > 1 {
-        for i in 2..(current + 1) {
-            if current % i == 0 {
-                current = current / i;
-                factors.push(i);
-                break;
-            }
-        }
+        let next_factor = smallest_divisor(current);
+        factors.push(next_factor);
+        current /= next_factor;
     }
     factors
+}
+
+pub fn smallest_divisor(number: u64) -> u64 {
+    for i in 2..(number + 1) {
+        if number % i == 0 {
+            return i
+        }
+    }
+    number
 }
